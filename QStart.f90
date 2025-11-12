70577bcc9b527c97bed5a38fb5989d4a1a385f0e
Matheus Moreira Martelli <matheusmmartelli@gmail.com>
2025-11-11 22:19:11 -0300

    Euler's Problem 3 Pt.2

--- QStart.f90
+++ QStart.f90
@@ -27,22 +27,26 @@
 
 function primeNumbers(k) result(PN)
     implicit none
-    integer, intent(in) :: k
-    integer, allocatable :: PN(:)
-    integer, allocatable :: PNM(:)
-    integer :: i, ii, j
+    integer, parameter :: TEST = SELECTED_INT_KIND(16)
+    integer(Kind=(TEST)), intent(in) :: k
+    integer(Kind=(TEST)), allocatable :: PN(:)
+    integer(Kind=(TEST)), allocatable :: PNM(:) !M stands for Maker
+    integer(Kind=(TEST)) :: i, ii, j
 
     allocate(PNM(k))!F processamento
     j=2
     PNM(1) = 1
     PNM(2) = 2
 
-    primeConstructor: do i = 1, k, 1
-        primecomputor: do ii = j, 2, -1
-            if((mod(i, PNM(ii)) == 0 .and. i>=PNM(ii)) .or. i==1)then
+    primeConstructor: do i = 2, k, 1
+        primecomputor: do ii = 2, j, 1
+            if((mod(i, PNM(ii)) == 0 .and. i>=PNM(ii)))then
                 cycle primeConstructor
             end if 
         end do primecomputor
+        !if(mod(j,1000) == 0)then
+            !print *, "to em", i
+        !end if 
         j = j+1
         PNM(j) = i
     end do primeConstructor
@@ -52,23 +56,58 @@ function primeNumbers(k) result(PN)
     do i=1, size(PN), 1
         PN(i) = PNM(i)
     end do
-
 end function primeNumbers
 
-program Euler3 !maior número primo divisor de 600851475143, de 13195 é 5, 7, 13, 29
+function factors(N,D) result(F) !Faz isso
     implicit none
-    integer :: number
-    integer, allocatable :: primes(:), primeFactors(:)
+    integer, parameter :: TEST = SELECTED_INT_KIND(16)
+    integer(Kind=(TEST)), intent(in) :: n
+    integer(Kind=(TEST)), allocatable, intent(in) :: D(:)
+    integer(Kind=(TEST)), allocatable :: F(:)
+    integer(Kind=(TEST)), allocatable :: FM(:)
+    integer(Kind=(TEST)) :: i,j
+    allocate(FM(size(D)))
+    FM(:) = 0
+    j=0
+
+    do i=1, size(D), 1
+        if(mod(n, D(i))  == 0 .and. n>D(i))then
+            j=j+1
+            FM(j) = D(i)
+        end if
+    end do
+
+    allocate(F(j))
+    do i=1, size(F), 1
+        F(i) = FM(i)
+    end do
+end function
+
+program Euler3 !maior número primo divisor de 600851475143, de 13195 é 5, 7, 13, 29, eu adicionei 1, pq sim
+    implicit none
+    integer, parameter :: TEST = SELECTED_INT_KIND(16)
+    integer(Kind=(TEST)) :: number
+    integer(Kind=(TEST)), allocatable :: primes(:), primeFactors(:)
     interface
         function primeNumbers(k) result(PN)
-            integer, intent(in) :: k
-            integer, allocatable :: PN(:)
-            integer, allocatable :: PNM(:)
-            integer :: i, ii, j
+            integer, parameter :: TEST = SELECTED_INT_KIND(16)
+            integer(Kind=(TEST)), intent(in) :: k
+            integer(Kind=(TEST)), allocatable :: PN(:)
+            integer(Kind=(TEST)), allocatable :: PNM(:)
+            integer(Kind=(TEST)) :: i, ii, j
+        end function
+    end interface
+    interface 
+        function factors(N,D) result(F)
+            integer, parameter :: TEST = SELECTED_INT_KIND(16)
+            integer(Kind=(TEST)), intent(in) :: n
+            integer(Kind=(TEST)), allocatable ,intent(in) :: D(:)
+            integer(Kind=(TEST)), allocatable :: F(:)
+            integer(Kind=(TEST)) :: i,j
         end function
     end interface
 
-    number = 64000
-    print *, "K"
-    print *, primeNumbers(number)
+    number = 600851475143_TEST
+    primes = primeNumbers(int8(number**0.5))
+    print *, factors(number, primes)
 end program Euler3
